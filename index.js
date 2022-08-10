daf49c27ac9a7793466c5c484bce3681e63e275d
tenmark86e <tenmark86@gmail.com>
2022-08-10 15:02:16 +0300

    Done Functions: Continued assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+/*  defines saturdayFun function expression as specified */
+
+function saturdayFun(activity="roller-skate") {
+
+    return `This Saturday, I want to ${activity}!`;
+
+  }
+  
+  /*  defines mondayWork function expression as specified */
+
+  let mondayWork = function(activity="go to the office") {
+
+    return `This Monday, I will ${activity}.`;
+  }
+  /*defines wrapAdjective function according to the specification` */
+
+  
+  let wrapAdjective = function(style="*") {
+
+    return function(adjective="special") {
+
+      return `You are ${style}${adjective}${style}!`;
+
+    }
+    
+  }
